74577930408657a8a5b16b83a8b29ae36d9bd19b
imrandevop <imranprvt11@gmail.com>
2025-05-17 20:47:02 +0530

    admin auth changed

--- users/models.py
+++ users/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
-        if not username:
-            raise ValueError('The Username field must be set')
+    def create_user(self, phone_number, name, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('The Phone Number field must be set')
         
-        user = self.model(username=username, **extra_fields)
+        # Generate a username from phone number if not provided
+        if not extra_fields.get('username'):
+            extra_fields['username'] = f"user_{phone_number}"
+        
+        user = self.model(
+            phone_number=phone_number,
+            name=name,
+            **extra_fields
+        )
         
         if password:
             user.set_password(password)
@@ -16,14 +24,20 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, username, password=None, **extra_fields):
+    def create_superuser(self, phone_number, name, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(username, password, **extra_fields)
-
+        
+        if not extra_fields.get('username'):
+            extra_fields['username'] = f"admin_{phone_number}"
+            
+        if password is None:
+            raise ValueError('Superuser must have a password')
+            
+        return self.create_user(phone_number, name, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, unique=True, null=True, blank=True)  # Allow null temporarily
     phone_number = models.CharField(max_length=15, unique=True)
     name = models.CharField(max_length=100)
     
@@ -33,8 +47,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     objects = UserManager()
     
-    USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['phone_number', 'name']
     
     def __str__(self):
         return f"{self.name} ({self.phone_number})"
+    
+    def save(self, *args, **kwargs):
+        # Auto-generate username if not provided
+        if not self.username:
+            self.username = f"user_{self.phone_number}"
+        super().save(*args, **kwargs)
