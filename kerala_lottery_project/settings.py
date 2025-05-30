25ba69029a170c5438cb8c77613fde5db74aa26e
imrandevop <imranprvt11@gmail.com>
2025-05-30 19:45:31 +0530

    correcting admin design

--- kerala_lottery_project/settings.py
+++ kerala_lottery_project/settings.py
@@ -261,11 +261,4 @@ FEATURE_FLAGS = {
     'ENABLE_ADMIN_HONEYPOT': not DEBUG,  # Consider adding django-admin-honeypot
 }
 
-# Celery settings (for future background tasks)
-if os.getenv('REDIS_URL'):
-    CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0')
-    CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0')
-    CELERY_ACCEPT_CONTENT = ['json']
-    CELERY_TASK_SERIALIZER = 'json'
-    CELERY_RESULT_SERIALIZER = 'json'
-    CELERY_TIMEZONE = TIME_ZONE
+
